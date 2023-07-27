2dc39e427091b9c6d82efef09d59df1d92068b96
Louis Mille <mille.louis1@gmail.com>
2023-07-27 18:12:36 +0200

    update

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Louis Mille on 25/07/2023.
+//
+
+import Foundation
+
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,92 +7,55 @@
 
 import SwiftUI
 
-//struct CustomText: View {
-//    let text: String
-//
-//    var body: some View {
-//        Text(text)
-//    }
-//
-//    init(_ text: String) {
-//        print("creating a new custom text")
-//        self.text = text
-//    }
-//}
-
-
-//struct User: Codable {
-//    let name: String
-//    let address: Address
-//}
-//
-//struct Address: Codable {
-//    let street: String
-//    let city: String
-//}
-
 struct ContentView: View {
-    let layout = [
-//        GridItem(.adaptive(minimum: 80, maximum: 120)),
-//        GridItem(.fixed(80)),
-//        GridItem(.fixed(80))
-//    ]
+    
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     
     var body: some View {
-//        ScrollView(.horizontal) {
-//            LazyHGrid(rows: layout) {
-//                ForEach(0..<1000) {
-//                    Text("item \($0)")
-//                }
-//            }
-//        }
-//
         
-        
-//        Button("Decode JSON") {
-//            let input = """
-//            {
-//                "name": "Taylor Swift",
-//                "address": {
-//                    "street": "555, Taylor Swift Avenue",
-//                    "city": "Nashville"
-//                }
-//            }
-//            """
-//
-//            let data = Data(input.utf8)
-//
-//            if let user = try? JSONDecoder().decode(User.self, from: data) {
-//                print(user.address.street)
-//            }
-//        }
-//        NavigationView {
-//            List(0..<100) { row in
-//                NavigationLink {
-//                    Text("Detail \(row)")
-//                } label: {
-//                    Text("Row \(row)")
-//                        .padding()
-//                        .navigationTitle("SwiftUI")
-//                }
-//            }
-//        }
-//        ScrollView(.horizontal) {
-//            LazyHStack(spacing: 10) {
-//                ForEach(0..<100) {
-//                    CustomText("Item \($0)")
-//                        .font(.title)
-//                }
-//                .frame(maxWidth: .infinity)
-//            }
-//        }
-//        GeometryReader { geo in
-//            Image("example")
-//                .resizable()
-//                .scaledToFit()
-//                .frame(width: geo.size.width * 1.0)
-//                .frame(width: geo.size.width, height: geo.size.height)
-//        }
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            MissionView(mission: mission, astronauts: astronauts)
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .padding()
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                        .foregroundColor(.white)
+                                    Text(mission.formattedLaunchDate)
+                                        .font(.caption)
+                                        .foregroundColor(.white.opacity(0.5))
+                                }
+                                .padding(.vertical)
+                                .frame(maxWidth: .infinity)
+                                .background(.lightBackground)
+                            }
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(.lightBackground)
+                            )
+                        }
+                    }
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .navigationTitle("Moonshot")
+            .background(.darkBackground)
+            .preferredColorScheme(.dark)
+        }
     }
 }
 
